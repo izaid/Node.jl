7991d17347e705505e24f2101dd642ba43ebe05f
izaid <hi@irwinzaid.com>
2017-03-06 14:44:59 +0000

    Updating build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,8 +1,11 @@
-cd("j2")
+run(`npm install git+https://github.com/izaid/j2.git`)
+cp(joinpath(dirname(@__FILE__), "node_modules/j2/build/Release/v8.a"), joinpath(dirname(@__FILE__), "usr/lib/"); remove_destination=true)
+
+#cd("j2")
 #mkdir("build")
-cd("build")
+#cd("build")
 
-path =  joinpath(dirname(@__FILE__), "usr")
+#path =  joinpath(dirname(@__FILE__), "usr")
 
-run(`cmake -DCMAKE_INSTALL_PREFIX=$path ..`)
-run(`make install`)
+#run(`cmake -DCMAKE_INSTALL_PREFIX=$path ..`)
+#run(`make install`)
