1383a592e0ed08a0a2cd97af619ed38d156644bf
izaid <hi@irwinzaid.com>
2017-03-06 12:13:01 +0000

    Added some basic tests

--- tests/test.jl
+++ tests/test.jl
@@ -6,3 +6,8 @@ using V8
 @test false === js"false"
 
 @test 0.5 === js"0.5"
+@test 1.0 === js"1.0"
+@test 1.5 === js"1.5"
+
+@test "Hello, world!" == js"\"Hello, world!\""
+@test "Hello, world!" != js"\"Hello, wo\""
