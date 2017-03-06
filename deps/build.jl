df49c854f84f604cfa4c662e279a1016cad634c9
izaid <hi@irwinzaid.com>
2017-03-06 14:55:04 +0000

    Updated build.jl

--- deps/build.jl
+++ deps/build.jl
@@ -1,11 +1,8 @@
 run(`npm install git+https://github.com/izaid/j2.git`)
-cp(joinpath(dirname(@__FILE__), "node_modules/j2/build/Release/v8.a"), joinpath(dirname(@__FILE__), "usr/lib/"); remove_destination=true)
 
-#cd("j2")
-#mkdir("build")
-#cd("build")
+if ispath("usr")
+    rm("usr"; recursive=true)
+end
+mkpath("usr/lib")
 
-#path =  joinpath(dirname(@__FILE__), "usr")
-
-#run(`cmake -DCMAKE_INSTALL_PREFIX=$path ..`)
-#run(`make install`)
+cp("node_modules/j2/build/Release/v8.a", "usr/lib/v8.a")
